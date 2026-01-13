af66472135bc2b7cda714b61458b749d00e66d2f
ahkmtrpi2016-a11y <ahkmtrpi2016@gmail.com>
2026-01-13 21:21:08 +0700

    Commit dan klik Push origin

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,12 +6,7 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
+    extend: {},
   },
   plugins: [],
-};
+}
